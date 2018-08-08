8c7a94d43fcd226ee3213c673a1c3ad847c2cd1d
Romeo Folie <romeofolie1@gmail.com>
2018-08-08 17:23:30 +0000

    cleaned fetcher up

--- fetcher/news.js
+++ fetcher/news.js
@@ -1,7 +1,9 @@
 const request = require('request-promise')
 const cheerio = require('cheerio')
 const _ = require('lodash')
-const { sendMail } = require('./../mailer/mailer')
+const {
+  sendMail
+} = require('./../mailer/mailer')
 
 
 var gnpcNews = "http://gnpcghana.com/news.html"
@@ -22,29 +24,13 @@ function checkHeadlines() {
         return $(x).text().trim()
       })
 
-      //so basically, what we are going to do is to check if the last headline on the list is
-      //what we have here. If it is, we'll move on and check again after like an hour
-      if (headlines[0] === "21.07.18 - CALL FOR (LOCAL) POST GRADUATE SCHOLARSHIP APPLICATIONS : GNPC FOUNDATION") {
-        //we'll just chill, smoke some weed and check again in like a day
-        console.log("Well it's the same old news. Pay your own fees motherfucker")
-      }
-      //The real magic happens when the headline isn't what we have in our list
-      //we first check if the date side contains 18
-      else {
-        //There's a new headline. Now let's check if it contains our keywords
-        if (_.includes(headlines[0], "UNDERGRADUATE") && _.includes(headlines[0], "SCHOLARSHIP")) {
-          //call function that sends mail to all users in the database
-          sendMail()
-          clearInterval(recheck)
-          console.log("Congratulations motherfucker")
-        }
-        //If it doesn't contain any of our keywords
-        //well we'll smoke some more weed and try again later
-        else {
-          console.log("Nigga, pay your own damn fees!")
-        }
+      if (headlines[0] !== "21.07.18 - CALL FOR (LOCAL) POST GRADUATE SCHOLARSHIP APPLICATIONS : GNPC FOUNDATION" && (_.includes(headlines[0], "UNDERGRADUATE") && _.includes(headlines[0], "SCHOLARSHIP"))) {
+        sendMail()
+        clearInterval(recheck)
       }
     }).catch(e => console.log(e))
 }
 
-module.exports = {checkHeadlines}
+module.exports = {
+  checkHeadlines
+}
